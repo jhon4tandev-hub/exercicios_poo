b2f00361e6863800b583c30f6026ab6d0b46bd3b
Jhonatan Silva <jhon4ta.n.dev@gmail.com>
2026-07-04 20:51:05 -0300

    criado os mettodos get e set

--- 02exercicio/src/livro.java
+++ 02exercicio/src/livro.java
@@ -5,6 +5,33 @@ public class livro {
     private int paginas;
     private boolean disponivel = true;
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        if (titulo.length() > 2)
+            this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        if (autor.length() > 2)
+            this.autor = autor;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        if (paginas > 0)
+            this.paginas = paginas;
+    }
+
     public void emprestar() {
         if (disponivel)
             disponivel = false;
